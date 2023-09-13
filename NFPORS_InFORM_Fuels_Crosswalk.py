71ff31f768d9bbe0a83ec0d266d58a6df12b02b0
Armstrong <warmstrong@ios.doi.gov>
2023-09-13 13:37:03 -0600

    Changed order of tables in dataframe

--- NFPORS_InFORM_Fuels_Crosswalk.py
+++ NFPORS_InFORM_Fuels_Crosswalk.py
@@ -3,44 +3,44 @@ import sys
 import pandas as pd
 
 # Replace these file paths with your actual file paths
-input_csv_path = 'input.csv'
-output_csv_path = 'output.csv'
+nfpors_table = r'C:\Users\warmstrong\Documents\work\InFORM\20230912 NFPORS InFORM Crosswalk Script\data input\hfr_allhazfuelsdata_doi_current.csv'
+inform_table = r'C:\Users\warmstrong\Documents\work\InFORM\20230912 NFPORS InFORM Crosswalk Script\data input\InFormFuelsFeatureCsvExtract BIA.csv'
 
 # Read the input CSV into a Pandas DataFrame
-df = pd.read_csv(input_csv_path)
+df = pd.read_csv(nfpors_table)
 
 # Define the mapping from the first table to the second table
 column_mapping = {
-    "Name": "ActivityTreatmentName",
-    "EstimatedTreatmentID": "ActivityTreatmentID",
-    "EstimatedActivityID": "d",
-    "LocalID": "TreatmentLocalIdentifier",
-    "Type": "TypeName",
-    "Category": "CategoryName",
+    "ActivityTreatmentName": "Name",
+    "ActivityTreatmentID": "EstimatedTreatmentID",
+    "ActivityTreatmentID": "EstimatedActivityID",  # Replace "d" with "ActivityTreatmentID"
+    "TreatmentLocalIdentifier": "LocalID",
+    "TypeName": "Type",
+    "CategoryName": "Category",
     "Class": "Class",
-    "Latitude": "TreatmentLatitude",
-    "Longitude": "TreatmentLongitude",
-    "CalculatedAcres": "PlannedAcres",
-    "IsWUI": "IsWui",
-    "InitiationDate": "PlannedInitiationDate",
-    "InitiationFiscalYear": "PlannedInitiationFiscalYear",
-    "InitiationFiscalQuarter": "PlannedInitiationFiscalQuarter",
+    "TreatmentLatitude": "Latitude",
+    "TreatmentLongitude": "Longitude",
+    "PlannedAcres": "CalculatedAcres",
+    "IsWui": "IsWUI",
+    "PlannedInitiationDate": "InitiationDate",
+    "PlannedInitiationFiscalYear": "InitiationFiscalYear",
+    "PlannedInitiationFiscalQuarter": "InitiationFiscalQuarter",
     "WBS": "WBS",
-    "FundingSource": "PlannedDirectCost",
-    "FundingDepartment": "DepartmentName",
-    "FundingAgency": "BureauName",
-    "FundingRegion": "RegionName",
-    "FundingUnit": "UnitName",
-    "FundingSubUnit": "SubUnitName",
-    "FundingUnitID": "NWCGUnitID",
-    "EstimatedPersonnelCost": "ForceAccountCost",
-    "EstimatedContractualCost": "ServiceContractCost",
-    "EstimatedGrantsFixedCost": "CoopAgreementCost",
-    "EstimatedTotalCost": "PlannedDirectCost",
-    "Notes": "ActivityTreatmentNotes",
+    "PlannedDirectCost": "FundingSource",
+    "DepartmentName": "FundingDepartment",
+    "BureauName": "FundingAgency",
+    "RegionName": "FundingRegion",
+    "UnitName": "FundingUnit",
+    "SubUnitName": "FundingSubUnit",
+    "NWCGUnitID": "FundingUnitID",
+    "ForceAccountCost": "EstimatedPersonnelCost",
+    "ServiceContractCost": "EstimatedContractualCost",
+    "CoopAgreementCost": "EstimatedGrantsFixedCost",
+    "PlannedDirectCost": "EstimatedTotalCost",
+    "ActivityTreatmentNotes": "Notes",
     "LocalApprovalDate": "LocalApprovalDate",
     "RegionalApprovalDate": "RegionalApprovalDate",
-    "AgencyApprovalDate": "BureauApprovalDate",
+    "BureauApprovalDate": "AgencyApprovalDate",
     "BILFunding": "BILFunding",
     "TreatmentDriver": "TreatmentDriver",
 }
@@ -49,6 +49,6 @@ column_mapping = {
 df.rename(columns=column_mapping, inplace=True)
 
 # Write the modified DataFrame to the output CSV
-df.to_csv(output_csv_path, index=False)
+df.to_csv(inform_table, index=False)
 
 print("CSV transformation complete.")
