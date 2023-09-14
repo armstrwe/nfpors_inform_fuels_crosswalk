b7604727b7d83c4f370384cf7f8b5cb6295cf0fd
Armstrong <warmstrong@ios.doi.gov>
2023-09-14 11:05:31 -0600

    Changed to use Excel NFPORS download

--- NFPORS_InFORM_Fuels_Crosswalk.py
+++ NFPORS_InFORM_Fuels_Crosswalk.py
@@ -1,19 +1,52 @@
+# Script will crosswalk data from NFPORS to InFORM Fuels table
+# Will be desinged to work with multiple bureaus data
+# Default will be to read an Excel file download from NFPORS
+# Can be modified to read a CSV file download from NFPORS
+#
+
+
 import os
 import sys
 import pandas as pd
 
 # Replace these file paths with your actual file paths
-nfpors_table = r'C:\Users\warmstrong\Documents\work\InFORM\20230912 NFPORS InFORM Crosswalk Script\data input\hfr_allhazfuelsdata_doi_current.csv'
+nfpors_table = r'C:\Users\warmstrong\Documents\work\InFORM\20230912 NFPORS InFORM Crosswalk Script\data input\hfr_allhazfuelsdata_doi_all.xlsx'
 inform_table = r'C:\Users\warmstrong\Documents\work\InFORM\20230912 NFPORS InFORM Crosswalk Script\data input\InFormFuelsFeatureCsvExtract BIA.csv'
 
-# Read the input CSV into a Pandas DataFrame
-df = pd.read_csv(nfpors_table)
 
-# Define the mapping from the first table to the second table
+
+#------------------------------------------------------------
+# CSV file encoding detection
+# List of possible encodings to try
+# encodings_to_try = ['utf-8', 'ISO-8859-1', 'latin1']
+
+# Try reading the input CSV using different encodings
+#df = None
+#for encoding in encodings_to_try:
+#    try:
+#        df = pd.read_csv(nfpors_table, encoding=encoding)
+#        break  # Break the loop if successful
+#    except UnicodeDecodeError:
+#        continue  # Try the next encoding if decoding fails
+
+# Check if the DataFrame was successfully loaded
+#if df is None:
+#    print("Error: Unable to read the CSV file with any encoding.")
+
+#------------------------------------------------------------
+
+df = pd.read_excel(nfpors_table)
+
+# Check if the DataFrame was successfully loaded
+if df is None:
+    print("Error: Unable to read the CSV file with any encoding.")
+
+
+# Mapping from NFPORS table to InFORM Fuels table
 column_mapping = {
     "ActivityTreatmentName": "Name",
     "ActivityTreatmentID": "EstimatedTreatmentID",
-    "ActivityTreatmentID": "EstimatedActivityID",  # Replace "d" with "ActivityTreatmentID"
+    "ActivityTreatmentID": "EstimatedActivityID",  
     "TreatmentLocalIdentifier": "LocalID",
     "TypeName": "Type",
     "CategoryName": "Category",
